f26c01988c102e9b113b8a41df214f2055e3364f
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2023-02-04 13:57:35 +0200

    render5

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3001
-const BUILD_DIR = '../frontend/build'
+const BUILD_DIR = 'frontend/build'
 
 app.use(express.static(BUILD_DIR))
 app.get('*', (req, res) => {
 	console.log(`Hello visitor on port ${process.env.PORT}`)
-	res.send(`${BUILD_DIR}/index.html`)
+	res.send(path.join(__dirname,'..', 'frontend', 'build', 'index.html'))
 })
 
 app.listen(PORT, () => {
