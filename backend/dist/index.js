ffd7a9865817a1234e0ba365848abcf72611a64c
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2023-02-04 14:34:29 +0200

    render8

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,14 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
-const BUILD_DIR = '../frontend/build';
+const BUILD_DIR = 'frontend/build';
 app.use(express_1.default.static(BUILD_DIR));
 app.get('*', (req, res) => {
     console.log(`Hello visitor on port ${process.env.PORT}`);
-    res.send(`${BUILD_DIR}/index.html`);
+    res.sendFile(path_1.default.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 app.listen(PORT, () => {
     console.log(`⚡️Listening to port ${PORT}`);
