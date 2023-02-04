ebcb88a11c278d86dc83b70fbec3eadbfddd4fa2
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2023-02-04 16:00:33 +0200

    04.02.2023-1

--- frontend/src/components/GameLayout.tsx
+++ frontend/src/components/GameLayout.tsx
@@ -5,18 +5,18 @@ type Props = {}
 
 const GameLayout = (props: Props) => {
 	return (
-		<div className="min-h-screen border-8 border-red-500 bg-gradient-to-b from-sky-100 to-pink-50 pt-16 font-comic-neue">
+		<div className="min-h-screen bg-gradient-to-b from-sky-100 to-pink-50 pt-16 font-comic-neue">
 			<div className="bg-white p-4">
-				<div className="grid grid-cols-3 text-xl text-neutral-800">
+				<div className="grid grid-cols-3 text-md sm:text-xl text-neutral-800">
 					<div>
-						<span className="font-bold">
+						<span className="font-bold hidden sm:visible">
 							Difficulty:&nbsp;
 						</span>
 						<span className="cursor-pointer font-extrabold text-neutral-400">
 							Easy
 						</span>
 					</div>
-					<div className="text-center font-bold">
+					<div className="text-center font-bold whitespace-nowrap">
 						Mistakes: 0/3
 					</div>
 					<div className="text-right font-extrabold text-neutral-400">
@@ -24,11 +24,11 @@ const GameLayout = (props: Props) => {
 					</div>
 				</div>
 			</div>
-			<div className="flex h-fit items-center justify-center border border-blue-600 p-4">
+			<div className="flex h-fit items-center justify-center p-4">
 				<img
 					src={sudokuBoardImg}
 					alt="Sudoku board test img"
-					className="h-96 w-96 border-2 border-black"
+					className="h-auto border border-black"
 				/>
 			</div>
 		</div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,17 +6,17 @@ type Props = {}
 
 const Navbar = (props: Props) => {
 	return (
-		<div className="absolute inset-x-0 flex h-16 items-center justify-between border-b border-b-neutral-200 bg-white px-8 text-center font-bold">
+		<div className="absolute inset-x-0 flex h-16 items-center justify-between border-b border-b-neutral-200 bg-white p-0 md:p-8 text-center font-bold">
 			<div className="flex items-center">
 				<FontAwesomeIcon
 					icon={faBars}
 					className="h-full w-8 cursor-pointer px-2 pt-1 text-purple-600"
 				/>
-				<span className="drop-shadow-black select-none bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text p-2 pl-4 font-permanent-marker text-4xl text-transparent drop-shadow-md">
+				<span className="drop-shadow-black select-none bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text p-2 pl-4 font-permanent-marker text-2xl text-transparent drop-shadow-md sm:text-4xl whitespace-nowrap">
 					Sudoku Pets
 				</span>
 			</div>
-			<div className="cursor-pointer bg-gradient-to-r from-blue-500 to-sky-500 bg-clip-text p-2 font-permanent-marker text-2xl text-transparent hover:from-blue-600 hover:to-sky-600">
+			<div className="cursor-pointer bg-gradient-to-r from-blue-500 to-sky-500 bg-clip-text p-2 font-permanent-marker text-lg sm:text-2xl text-transparent hover:from-blue-600 hover:to-sky-600">
 				New Game
 			</div>
 		</div>
