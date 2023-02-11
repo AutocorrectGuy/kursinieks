8edf59e7e9f81bf3a8fa163eb3344e1688a4ae77
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2023-02-11 14:19:54 +0200

    test-123

--- frontend/src/pages/subjects/Informatika/SudokuBoard.tsx
+++ frontend/src/pages/subjects/Informatika/SudokuBoard.tsx
@@ -6,15 +6,10 @@ type SudokuBoardType = {
 }
 
 const grid = [
-	-1, 4, 5, 9, -1, -1, 2, 1, -1,
-	8, -1, 2, 5, 1, 7, 4, -1, -1,
-	1, -1, 6, -1, -1, 4, -1, -1, 3,
-	-1, 8, 4, 7, -1, 2, -1, -1, 1,
-	2, 5, -1, 3, -1, 1, 6, 4, 9,
-	-1, -1, -1, 4, 5, -1, -1, 2, -1,
-	5, 6, 1, -1, 9, 8, -1, 8, 4,
-	4, 2, 9, 6, 7, -1, 1, -1, -1,
-	3, 7, 8, 1, 4, 5, -1, -1, -1,
+	-1, 4, 5, 9, -1, -1, 2, 1, -1, 8, -1, 2, 5, 1, 7, 4, -1, -1, 1, -1, 6, -1, -1, 4, -1,
+	-1, 3, -1, 8, 4, 7, -1, 2, -1, -1, 1, 2, 5, -1, 3, -1, 1, 6, 4, 9, -1, -1, -1, 4, 5, -1,
+	-1, 2, -1, 5, 6, 1, -1, 9, 8, -1, 8, 4, 4, 2, 9, 6, 7, -1, 1, -1, -1, 3, 7, 8, 1, 4, 5,
+	-1, -1, -1,
 ]
 const SudokuBoard = ({ cellSize }: SudokuBoardType) => {
 	const [highlighted, setHighlighted] = useState<number[]>([])
@@ -25,7 +20,7 @@ const SudokuBoard = ({ cellSize }: SudokuBoardType) => {
 			{grid.map((num, i) => (
 				<SudokuCell
 					key={`sudoku-cell-${i}`}
-					currentValue={num!==-1 ? num : null}
+					currentValue={num !== -1 ? num : null}
 					i={i}
 					isHighlighted={highlighted.includes(i)}
 					isSelected={i === selected}
