0721ffd1868d7122b37720a1d744f153fd6f69a3
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2023-02-12 08:15:50 +0200

    another MIME error fix

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,14 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
-const BUILD_PATH = path_1.default.join(__dirname, 'build/');
+const BUILD_PATH = __dirname + '/build/';
 app.use(express_1.default.static(BUILD_PATH));
 app.get('*', (req, res) => {
-    res.sendFile(path_1.default.resolve(BUILD_PATH, 'index.html'));
+    console.log(BUILD_PATH);
+    res.sendFile(BUILD_PATH + 'index.html');
 });
 app.listen(PORT, () => {
     console.log(`⚡️Listening to port ${PORT}`);
--- backend/index.ts
+++ backend/index.ts
@@ -5,11 +5,12 @@ import path from 'path'
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3001
-const BUILD_PATH = path.join(__dirname, 'build/')
+const BUILD_PATH = __dirname + '/build/'
 
 app.use(express.static(BUILD_PATH))
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(BUILD_PATH, 'index.html'))
+	console.log(BUILD_PATH)
+	res.sendFile(BUILD_PATH + 'index.html')
 })
 
 app.listen(PORT, () => {
