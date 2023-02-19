54d8763dfd5deec92a6e9a5c1bb3308f3afb787e
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2023-02-19 14:26:31 +0200

    express.static 2

--- backend/index.ts
+++ backend/index.ts
@@ -6,7 +6,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(`${__dirname}/build`))
+app.use(express.static(`${__dirname}/build/`))
 
 app.get('*', (req, res) => {
 	res.sendFile(`${__dirname}/build/index.html`)
